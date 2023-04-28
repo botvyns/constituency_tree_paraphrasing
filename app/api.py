d648bd1cfe7bf9b3452e63c03caf2c18daf45e39
botvyns <105560859+botvyns@users.noreply.github.com>
2023-04-28 20:28:25 +0300

    Adding some explanations

--- app/api.py
+++ app/api.py
@@ -23,25 +23,30 @@ def paraphrase(tree: str, limit=20) -> dict:
   paraphrased trees in json format"""
   
   tree = str(tree)
-
+  
+  # substitute Noun Phrases that are part of a larger NP with three dots
   tree_np_replaced = re.sub(r'\(NP \((?!NP).*?\) \)', '...', tree)
-
+  
+  # create a list of all Noun Phrases that are part of a larger NP 
   nps = re.findall(r'\(NP \((?!NP).*?\) \)', tree)
-
+  
+  # create all possible combinations of all extracted NP
   all_permutations = list(permutations(nps))
 
+  # go through each combination and substitute the dots in the given tree successively with each of the combination member 
   all_variants = []
-
+  
   for permutation in all_permutations:
     variant = tree_np_replaced[:]
     for constituent in permutation:
       variant = variant.replace("...", constituent, 1)
     all_variants.append(variant)
-
+  
+  # create dictionary with all resulted trees
   result = {"paraphrases": []}
 
   for i in all_variants[:limit]:
     result["paraphrases"].append({"tree": i})
-
+  # represent to the user in json format
   json_result = json.dumps(result, indent=4, default=str)
   return Response(content=json_result, media_type='application/json')
